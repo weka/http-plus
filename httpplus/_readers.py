e44e8e3e1026b9af7202433f06e927d43df49a11
Augie Fackler <raf@durin42.com>
2013-09-20 09:34:39 -0400

    _readers: reflow docstring to avoid too-long line

--- httpplus/_readers.py
+++ httpplus/_readers.py
@@ -98,11 +98,12 @@ class AbstractReader(object):
         return result
 
     def readto(self, delimstr, blocks = None):
-        """return available data chunks up to the first one in which delimstr
-        occurs. No data will be returned after delimstr -- the chunk in which
-        it occurs will be split and the remainder pushed back onto the available
-        data queue. If blocks is supplied chunks will be added to blocks, otherwise
-        a new list will be allocated.
+        """return available data chunks up to the first one in which
+        delimstr occurs. No data will be returned after delimstr --
+        the chunk in which it occurs will be split and the remainder
+        pushed back onto the available data queue. If blocks is
+        supplied chunks will be added to blocks, otherwise a new list
+        will be allocated.
         """
         if blocks is None:
             blocks = []
